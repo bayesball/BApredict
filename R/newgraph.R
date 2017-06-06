f85c2dcd23b1619fda4f7ae33b65b049c9685d28
Jim Albert <albertcb1@gmail.com>
2017-06-06 09:00:46 -0400

    new function

---
+++ R/newgraph.R
@@ -0,0 +1,16 @@
+newgraph <- function(out){
+  d1 <- data.frame(SO.Rate = out$S$SO / out$S$AB,
+                 BABIP.Rate = out$S$H.HR / out$S$AB.SO.HR,
+                 AVG = out$S$H / out$S$AB1,
+                 Type="Observed")
+  d2 <- data.frame(SO.Rate = out$S$SO.Rate,
+                 BABIP.Rate = out$S$H.Rate,
+                 AVG = out$S$Comp.Est,
+                 Type="Predicted")
+  d <- rbind(d1, d2)
+  d$BA <- ifelse(d$AVG > .3, "AVG > .300", "AVG <= .300")
+  ggplot(d, aes(SO.Rate, BABIP.Rate, color=BA)) + 
+    geom_point() +
+    facet_wrap(~ Type, ncol=1) +
+    coord_equal()
+}
