93b69e0812c608e7a21c3b801ee2211b47d855af
Jim Albert <albertcb1@gmail.com>
2018-04-03 13:39:21 -0400

    adjust reading in data code

--- R/collect_hitting_data.R
+++ R/collect_hitting_data.R
@@ -1,13 +1,10 @@
 collect_hitting_data <- function(){
-  d1 <- htmltab("https://www.si.com/mlb/stats", which=1)
-  d1 <- mutate(d1,
-               AB = as.numeric(AB),
-               H = as.numeric(H),
-               HR = as.numeric(HR),
-               K = as.numeric(K),
-               BB = as.numeric(BB))
-  d1$playerID <- paste(1:(dim(d1)[1]),
-                       d1$Player, sep=" ")
+  read_html("https://www.si.com/mlb/stats") %>% 
+    html_table(fill = TRUE) -> d
+  d1 <- d[[1]]
+  names(d1)[13] <- "AVG"
   names(d1)[11] <- "SO"
-  dplyr::select(d1, playerID, Team, AB, H, HR, SO, BB)
+#  d1$playerID <- paste(1:(dim(d1)[1]),
+#                      d1$Player, sep=" ")
+  dplyr::select(d1, Player, Team, AB, H, HR, SO, BB)
 }
